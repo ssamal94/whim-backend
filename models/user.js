1ac1c8ede835080abd6110d9fe29f91806cdf9e6
swagatsamal <sinu2290@gmail.com>
2022-04-07 16:56:58 -0400

    product table added

--- models/user.js
+++ models/user.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  aboutAuthor: String,
+  introAuthor: String,
+  isSubscribed: Boolean,
+  paidFor: Object,
+  posts: Object,
 });
 
 //Model Creation
